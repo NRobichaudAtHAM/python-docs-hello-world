17066a0e76a85819d3aa6ee7ab37098e85358500
Nathaniel Robichaud <nrobichaud@healthaffiliatesmaine.com>
2021-07-09 11:51:27 -0400

    test

--- app.py
+++ app.py
@@ -3,12 +3,8 @@ app = Flask(__name__)
 
 @app.route("/index")
 def index():
-    return {
-        "ya done goofed, this is not a page. "
-    }
+    return "ya done goofed, this is not a page."
 
 @app.route("/test_page")
 def test_page():
-    return {
-        "this is a test page!"
-    }
+    return "this is a test page!"
